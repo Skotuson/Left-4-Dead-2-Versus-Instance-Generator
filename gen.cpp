d5325a1df813bab036b610961368e7f786d5018b
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-02-16 19:11:10 +0100

    initial commit

---
+++ gen.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+template <class T_>
+std::vector<T_> Fisher_Yates ( const std::vector<T_> & cards ) {
+    
+}
+
+int main ( void ) {
+
+    return 0;
+}
