c653d6a9f22918c78aeb287025028bc311856ed6
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-02-16 19:31:18 +0100

    add fischer-yates impl

--- gen.cpp
+++ gen.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <ctime>
+#include <cstdlib>
+#include <unistd.h>
+
+template <class T_>
+void Print ( const std::vector<T_> & v ) {
+    for ( const auto & x : v ) 
+        std::cout << x << " ";
+    std::cout << std::endl;
+}
 
 template <class T_>
 std::vector<T_> Fisher_Yates ( const std::vector<T_> & cards ) {
-    
+    srand ( time ( nullptr ) );
+    std::vector<T_> shuffled = cards;
+    for ( size_t i = 0; i < cards . size(); i++ ) {
+        size_t j = rand() % ( i + 1 );
+        T_ tmp = shuffled[i];
+        shuffled[i] = shuffled[j];
+        shuffled[j] = tmp;
+    }
+    return shuffled;
 }
 
 int main ( void ) {
-
+    //std::vector<std::string> a = { "Francis", "Louis", "Rochelle", "Zoey", "Bill", "Nick", "Ellis", "Coach" };
+    //Print ( a );
+    //for ( size_t i = 0; i < 15; i++ ) {
+    //    usleep ( 1'000'000 );
+    //    Print<std::string> ( Fisher_Yates<std::string> ( a ) );
+    //}
     return 0;
 }
