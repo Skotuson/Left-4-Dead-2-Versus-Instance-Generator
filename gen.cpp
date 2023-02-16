d8a3e9d29d456df2690c6afb4ab80cb8ab1fab7d
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-02-16 19:51:07 +0100

    add basic print out

--- gen.cpp
+++ gen.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <string>
 #include <ctime>
 #include <cstdlib>
 #include <unistd.h>
+#include <cmath>
 
 template <class T_>
 void Print ( const std::vector<T_> & v ) {
@@ -26,6 +28,45 @@ std::vector<T_> Fisher_Yates ( const std::vector<T_> & cards ) {
 }
 
 int main ( void ) {
+    std::ifstream ifs ( "in.txt" );
+    size_t team_one, team_two, team_total;
+
+    ifs >> team_one >> team_two;
+    team_total = team_one + team_two;
+
+    if ( team_total > 8 ) {
+        team_one = 4;
+        team_two = 4;
+    }
+
+    else if ( team_total < 8 ) {
+        srand ( time ( nullptr ) );
+        int coin = rand() % 2;
+        if ( ! coin ) {
+            team_one = std::ceil ( team_total / 2.0 );
+            team_two = std::floor ( team_total / 2.0 );
+        }
+    }
+
+    std::vector<std::string> players;
+    while ( team_total-- ) {
+        std::string player;
+        ifs >> player;
+        players . push_back ( player );
+    }
+
+    std::vector<std::string> players_shuffled = Fisher_Yates ( players );
+
+    std::cout << "Survivors:" << std::endl; 
+    for ( size_t i = 0; i < team_one; i++ ) 
+        std::cout << players_shuffled[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "Infected:" << std::endl; 
+    for ( size_t i = team_one; i < team_one + team_two; i++ ) 
+        std::cout << players_shuffled[i] << " ";
+    std::cout << std::endl;
+
     //std::vector<std::string> a = { "Francis", "Louis", "Rochelle", "Zoey", "Bill", "Nick", "Ellis", "Coach" };
     //Print ( a );
     //for ( size_t i = 0; i < 15; i++ ) {
