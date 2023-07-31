247f438cce953c56b7d713359b001ffd257d83fd
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-07-31 23:20:13 +0200

    update Database

--- Database.cpp
+++ Database.cpp
@@ -1,1 +1,17 @@
+#include <fstream>
+
 #include "Database.h"
+
+Database::Database ( const std::string & filename )
+: m_Source ( filename )
+{}
+
+void Database::Set_Source ( const std::string & filename ) {
+    m_Source = filename;
+}
+
+void Database::Load ( void ) {
+    std::ifstream ifs ( m_Source );
+
+    ifs . close ( );
+}
--- Database.h
+++ Database.h
@@ -1,10 +1,20 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <map>
+#include <set>
+#include <string>
+#include <cstdlib>
+
 class Database {
     public:
-        
+             Database   ( const std::string & filename );
+        void Set_Source ( const std::string & filename );
+        void Load       ( void );
     private:
+        std::string                             m_Source;
+        std::map<std::string, size_t>           m_Players;
+        std::map<std::set<std::string>, size_t> m_Teams;
 };
 
 #endif
