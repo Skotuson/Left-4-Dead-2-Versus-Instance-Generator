ca6cc58ca7ecd4d6b2af63cbc3bd2531e50bfb4d
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-08-01 00:07:06 +0200

    add helper

---
+++ Helper.cpp
@@ -0,0 +1,13 @@
+bool is_numeric ( const std::string & str ) {
+    for ( const auto & c : str )
+        if ( ! isdigit ( c ) )
+            return false;
+    return true;
+}
+
+void print_set ( const std::set<std::string> & Set ) {
+    std::cout << "{ ";
+    for ( const auto & elem : Set )
+        std::cout << elem << " ";
+    std::cout << "}";
+}
---
+++ Helper.h
@@ -0,0 +1,9 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <string>
+
+bool is_numeric ( const std::string           & str );
+void print_set  ( const std::set<std::string> & Set );
+
+#endif
