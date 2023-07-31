64d20bc1bcc7807936dc93c1e56c2b94a3e639c6
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-07-31 23:44:08 +0200

    update database

--- Database.cpp
+++ Database.cpp
@@ -1,12 +1,10 @@
-#include <fstream>
-
 #include "Database.h"
 
 Database::Database ( const std::string & filename )
 : m_Source ( filename )
 {}
 
-void Database::Set_Source ( const std::string & filename ) {
+void Database::SetSource ( const std::string & filename ) {
     m_Source = filename;
 }
 
@@ -14,4 +12,50 @@ void Database::Load ( void ) {
     std::ifstream ifs ( m_Source );
 
     ifs . close ( );
+}
+
+size_t Database::GetGames ( void ) {
+    return m_Games;
+}
+
+bool Database::ParseEntry ( std::ifstream & ifs ) {
+    std::string tok;
+    std::set<std::string> team_a, 
+                          team_b;
+    bool team = false,
+         colon = false;
+    
+    while ( ifs >> tok ) {
+        //Teams delimiter parsed
+        if ( tok == "x" ) team = true;
+        //Teams and score delimiter parsed
+        else if ( tok == "-" ) continue; 
+
+        //lexed number
+        else if ( is_numeric ( tok ) ) {
+            //second team score
+            size_t val = std::stoi ( tok );
+            m_Games += val;
+            if ( colon ) {
+                //update_players_score ( team_b, players_score, val );
+                //update_team_score ( teams_score, team_b, val );
+                return true;
+            } 
+            else {
+                //update_players_score ( team_a, players_score, val );
+                //update_team_score ( teams_score, team_a, val );
+            }
+        }
+
+        //Score delimiter parsed
+        else if ( tok == ":" ) colon = true;
+
+        else {
+            if ( team ) team_b . insert ( tok );
+            else team_a . insert ( tok );
+        }    
+    }
+    
+    //All entries were parsed
+    return false;
 }
--- Database.h
+++ Database.h
@@ -4,17 +4,22 @@
 #include <map>
 #include <set>
 #include <string>
+#include <fstream>
 #include <cstdlib>
 
 class Database {
     public:
-             Database   ( const std::string & filename );
-        void Set_Source ( const std::string & filename );
-        void Load       ( void );
+               Database   ( const std::string & filename );
+        void   SetSource  ( const std::string & filename );
+        void   Load       ( void );
+        size_t GetGames   ( void );
     private:
+        bool   ParseEntry ( std::ifstream     & ifs );
+
         std::string                             m_Source;
         std::map<std::string, size_t>           m_Players;
         std::map<std::set<std::string>, size_t> m_Teams;
+        size_t                                  m_Games    = 0;
 };
 
 #endif
