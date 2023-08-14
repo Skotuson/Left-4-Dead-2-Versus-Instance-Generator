66f5e07abd4505826449400ded07daf52a331653
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2023-08-14 22:39:52 +0200

    Update REPL.cpp

--- REPL.cpp
+++ REPL.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <string>
 
 #include "REPL.h"
 
 void REPL::Run ( void ) {
-    std::cout << "Welcome to the L4D2 Instance Generator interactive mode. Use the \"help\" to learn more commands" << std::endl;
-    std::cout << " > ";
+    std::cout << "Welcome to the L4D2 Instance Generator interactive mode.\nUse the \"help\" to learn more commands" << std::endl;
     while ( Read ( ) ) {
         Eval ( );
     }
 }
 
 bool REPL::Read ( void ) {
-
+    std::string cmnd;
+    do {
+        std::cout << "> ";
+    } while ( std::cin >> cmnd && cmnd != "exit" );
     return false;
 }
 
