994a21bca0290eee97d1dd8c4a99422d6e8fb5a3
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-01 23:25:19 +0100

    update team_t interface

--- src/team/team_t.cpp
+++ src/team/team_t.cpp
@@ -1,1 +1,6 @@
 #include "team_t.h"
+
+void team_t::player_add(player_t const &player)
+{
+    teammates_.insert(player);
+}
--- src/team/team_t.h
+++ src/team/team_t.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include <set>
+
+#include "../player/player_t.h"
+
 struct team_t
 {
-    
+    void player_add(player_t const &player);
+
+    using iterator_ = std::set<player_t>::iterator;
+    using c_iterator_ = std::set<player_t>::const_iterator;
+
+    iterator_ begin() { return teammates_.begin(); }
+    iterator_ end() { return teammates_.end(); }
+
+    c_iterator_ begin() const { return teammates_.begin(); }
+    c_iterator_ end() const { return teammates_.end(); }
+
+    c_iterator_ cbegin() const { return teammates_.cbegin(); }
+    c_iterator_ cend() const { return teammates_.cend(); }
+
+private:
+    std::set<player_t> teammates_;
 };
