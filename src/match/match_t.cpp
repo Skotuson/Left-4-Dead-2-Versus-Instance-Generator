b737b067e0c1c715ec889add8acf55646f1c79bf
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-01 22:48:48 +0100

    rename victory_ -> outcome_

--- src/match/match_t.cpp
+++ src/match/match_t.cpp
@@ -1,9 +1,9 @@
 #include "match_t.h"
 
-match_t::match_t(team_t const &team_a, team_t const &team_b, bool victory, match_t::timestamp_t const &timestamp)
+match_t::match_t(team_t const &team_a, team_t const &team_b, bool outcome, match_t::timestamp_t const &timestamp)
     : team_a_(team_a),
       team_b_(team_b),
-      victory_(victory),
+      outcome_(outcome),
       timestamp_(timestamp)
 {
 }
--- src/match/match_t.h
+++ src/match/match_t.h
@@ -8,14 +8,14 @@ struct match_t
 {
     using timestamp_t = std::chrono::system_clock::time_point;
 
-    match_t(team_t const &team_a, team_t const &team_b, bool victory, timestamp_t const & timestamp = std::chrono::system_clock::now());
+    match_t(team_t const &team_a, team_t const &team_b, bool outcome, timestamp_t const &timestamp = std::chrono::system_clock::now());
 
 private:
     team_t const team_a_;
     team_t const team_b_;
 
     // 0 signals that team A won, 1 signals that team B won
-    bool victory_;
+    bool outcome_;
 
     std::chrono::system_clock::time_point timestamp_;
 };
