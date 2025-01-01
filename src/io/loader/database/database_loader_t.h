bf7202286cc963be2072d181b683c6210f566785
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-01 23:06:25 +0100

    add regex format variable

--- src/io/loader/database/database_loader_t.h
+++ src/io/loader/database/database_loader_t.h
@@ -3,6 +3,15 @@
 #include "../loader_t.h"
 #include "../../store/database_t.h"
 
+#include <regex>
+
 struct database_loader_t : loader_t<database_t>
 {
+    database_loader_t(std::regex const &format)
+        : format_(format)
+    {
+    }
+
+protected:
+    std::regex format_;
 };
--- src/io/loader/database/old_format_database_loader_t.h
+++ src/io/loader/database/old_format_database_loader_t.h
@@ -2,6 +2,12 @@
 
 #include "database_loader_t.h"
 
+#include <regex>
+
 struct old_format_database_loader_t : database_loader_t
 {
+    old_format_database_loader_t(void)
+        : database_loader_t(std::regex(R"((.+?)\s+x\s+(.+?)\s+-\s+(\d+)\s*:\s*(\d+))"))
+    {
+    }
 };
