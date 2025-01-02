e06c1d27c54c5734e70b382035e28ceab1f64ed3
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-02 20:06:59 +0100

    Update text_stream_old_format_database_saver_t.cpp

--- src/io/saver/database/stream/text_stream_old_format_database_saver_t.cpp
+++ src/io/saver/database/stream/text_stream_old_format_database_saver_t.cpp
@@ -4,7 +4,7 @@
 #include <map>
 
 text_stream_old_format_database_saver_t::text_stream_old_format_database_saver_t(std::string const &filename)
-    : stream_old_format_database_saver_t(std::move(std::ofstream(filename)))
+    : stream_old_format_database_saver_t(std::make_unique<std::ofstream>(filename))
 {
 }
 
@@ -14,13 +14,26 @@ void text_stream_old_format_database_saver_t::save(database_t const &database)
     for (auto const &match : database.matches_get())
     {
         auto team_pair = std::make_pair(match.first(), match.second());
-        std::pair<size_t, size_t> win_pair;
 
         auto it = mapping.find(team_pair);
-        if(it == mapping.end())
+        if (it == mapping.end())
         {
-            mapping.insert({team_pair, {}});
+            mapping.insert({team_pair, {match.outcome() == match_t::outcome_t::FIRST_TEAM_WON, match.outcome() == match_t::outcome_t::FIRST_TEAM_WON}});
         }
-        
+
+        else if (match.outcome() == match_t::outcome_t::FIRST_TEAM_WON)
+        {
+            it->second.first += 1;
+        }
+
+        else if (match.outcome() == match_t::outcome_t::SECOND_TEAM_WON)
+        {
+            it->second.second += 1;
+        }
+    }
+
+    for (auto const &record : mapping)
+    {
+        *underlying_ << record.first.first << " x " << record.first.second << " - " << record.second.first << " : " << record.second.first << std::endl;
     }
 }
