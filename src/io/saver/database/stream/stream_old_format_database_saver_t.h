73b29ad8204fc79a28902181a17e354fd96f5f7f
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-02 20:05:43 +0100

    add ostream uniq_ptr

--- src/io/saver/database/stream/stream_old_format_database_saver_t.h
+++ src/io/saver/database/stream/stream_old_format_database_saver_t.h
@@ -3,14 +3,15 @@
 #include "../old_format_database_saver_t.h"
 
 #include <ostream>
+#include <memory>
 
 struct stream_old_format_database_saver_t : old_format_database_saver_t
 {
-    stream_old_format_database_saver_t(std::ostream &&os)
-        : underlying_(os)
+    stream_old_format_database_saver_t(std::unique_ptr<std::ostream> os)
+        : underlying_(std::move(os))
     {
     }
 
 protected:
-    std::ostream &underlying_;
+    std::unique_ptr<std::ostream> underlying_;
 };
