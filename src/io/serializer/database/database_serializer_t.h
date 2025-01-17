f29dc1bcac1d61ad1b9c172b274d254f7a85f873
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-17 14:10:52 +0100

    serializer

--- src/io/serializer/database/database_serializer_t.h
+++ src/io/serializer/database/database_serializer_t.h
@@ -4,6 +4,6 @@
 #include "../../store/database_t.h"
 
 template <typename U>
-struct database_serializer_t : serializer_t<database_t, U>
+struct database_serializer_t : serializer_t<database_t const &, U>
 {
 };
