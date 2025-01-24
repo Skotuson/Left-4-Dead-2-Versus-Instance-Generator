5cd2aaa9f1be92183a5cdef38b6879e0ff4bf1d6
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-24 15:19:24 +0100

    database_deserializer_t : fix path

--- src/io/deserializer/database/database_deserializer_t.h
+++ src/io/deserializer/database/database_deserializer_t.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "../deserializer_t.h"
-#include "../../store/database_t.h"
+#include "../../../store/database_t.h"
 
 template <typename U>
 struct database_deserializer_t : deserializer_t<database_t, U>
