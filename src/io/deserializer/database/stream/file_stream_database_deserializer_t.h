264aa2db1de007bb5c6bc411b13194363c629fad
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-24 14:26:48 +0100

    create deserializer hierarchy

---
+++ src/io/deserializer/database/stream/file_stream_database_deserializer_t.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "stream_database_deserializer_t.h"
+
+struct file_stream_database_deserializer_t : stream_database_deserializer_t
+{
+    
+};
---
+++ src/io/deserializer/database/stream/stream_database_deserializer_t.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../database_deserializer_t.h"
+
+#include <istream>
+
+struct stream_database_deserializer_t : database_deserializer_t<std::istream>
+{
+};
