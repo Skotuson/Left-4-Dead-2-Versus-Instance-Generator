9026a0a1ee3d2c2c1e1c51a397424cc4402f680a
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-24 19:56:52 +0100

    remove unused loader

--- src/cli/controller/controller_t.h
+++ src/cli/controller/controller_t.h
@@ -5,11 +5,8 @@
 #include <string>
 #include <memory>
 
-#include "../../io/loader/database/database_loader_t.h"
-
 struct controller_t
 {
 
 private:
-    std::shared_ptr<database_loader_t> db_loader_;
 };
--- src/io/loader/database/new_format_database_loader_t.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "database_loader_t.h"
-
-struct new_format_database_loader_t : database_loader_t
-{
-};
