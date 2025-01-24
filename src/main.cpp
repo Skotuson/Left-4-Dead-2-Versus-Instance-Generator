dfd4f2251441f19abdac1512b430d6d598b722c1
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-24 15:31:56 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,10 @@ int main(int argc, char *argv[])
 {
     database_t db;
     std::ifstream ifs("persistence/save.txt");
-    std::shared_ptr<stream_database_deserializer_t> loader(new old_format_stream_database_deserializer_t());
+    std::shared_ptr<stream_database_deserializer_t> deserializer(new old_format_stream_database_deserializer_t());
     std::shared_ptr<database_saver_t> saver(new text_stream_old_format_database_saver_t("persistence/matches_old_format.txt"));
 
-    db = loader->deserialize(ifs);
+    db = deserializer->deserialize(ifs);
     ifs.close();
     saver->save(db);
     return 0;
