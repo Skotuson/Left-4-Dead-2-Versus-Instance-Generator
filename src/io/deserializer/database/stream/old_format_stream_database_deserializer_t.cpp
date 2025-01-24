e2b8a4055ef3d264531d1cedf615f3984bb7f301
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-24 15:28:56 +0100

    implement deserialize for old format

--- src/io/deserializer/database/stream/old_format_stream_database_deserializer_t.cpp
+++ src/io/deserializer/database/stream/old_format_stream_database_deserializer_t.cpp
@@ -1,8 +1,59 @@
 #include "old_format_stream_database_deserializer_t.h"
 
+#include <regex>
+
 database_t old_format_stream_database_deserializer_t::deserialize(std::istream &is) const
 {
-    database_t r;
+    database_t db;
+    std::string line;
+    while (std::getline(is, line))
+    {
+        // Strip windows linefeed
+        if (line.back() == '\r')
+        {
+            line.pop_back();
+        }
 
-    return r;
+        std::smatch match;
+
+        if (std::regex_match(line, match, database_t::OLD_DB_FORMAT))
+        {
+            auto collect_team = [&](std::string const &str)
+            {
+                team_t team;
+                std::istringstream iss(str);
+                player_t::player_id_t player_id;
+                while (iss >> player_id)
+                {
+                    team.player_add(player_t(player_id));
+                }
+
+                db.team_add(team);
+                return team;
+            };
+
+            auto team_a = collect_team(match[1].str());
+            auto team_b = collect_team(match[2].str());
+            size_t wins_a = std::stoi(match[3].str());
+            size_t wins_b = std::stoi(match[4].str());
+
+            auto register_matches = [&](team_t const &team_a, team_t const &team_b, match_t::outcome_t outcome, size_t n_games)
+            {
+                while (n_games--)
+                {
+                    db.match_add(match_t(team_a, team_b, outcome));
+                }
+            };
+
+            register_matches(team_a, team_b, match_t::outcome_t::FIRST_TEAM_WON, wins_a);
+            register_matches(team_a, team_b, match_t::outcome_t::SECOND_TEAM_WON, wins_b);
+        }
+
+        else
+        {
+            // TODO: add error handling
+        }
+    }
+
+    return db;
 }
