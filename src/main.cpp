b6acefd7c7a35c34491626a224ff4258ee919fd3
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-24 15:29:16 +0100

    use deserializer

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <cstring>
+#include <fstream>
 #include <memory>
 
 #include "store/database_t.h"
-#include "io/loader/database/database_loader_t.h"
+
+#include "io/deserializer/database/stream/stream_database_deserializer_t.h"
+#include "io/deserializer/database/stream/old_format_stream_database_deserializer_t.h"
+
 #include "io/saver/database/database_saver_t.h"
-#include "io/loader/database/stream/text_stream_old_format_database_loader_t.h"
 #include "io/saver/database/stream/text_stream_old_format_database_saver_t.h"
 
 int main(int argc, char *argv[])
 {
     database_t db;
-    std::shared_ptr<database_loader_t> loader(new text_stream_old_format_database_loader_t("persistence/save.txt"));
+    std::ifstream ifs("persistence/save.txt");
+    std::shared_ptr<stream_database_deserializer_t> loader(new old_format_stream_database_deserializer_t());
     std::shared_ptr<database_saver_t> saver(new text_stream_old_format_database_saver_t("persistence/matches_old_format.txt"));
 
-    db = loader->load();
+    db = loader->deserialize(ifs);
+    ifs.close();
     saver->save(db);
     return 0;
 }
