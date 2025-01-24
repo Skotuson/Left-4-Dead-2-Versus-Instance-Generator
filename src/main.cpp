3e0b637e0a06d832a7c79efa6f9983c70d52cc02
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-24 15:41:41 +0100

    add arg parsing boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 #include <cstring>
 #include <fstream>
 #include <memory>
+#include <string>
+#include <list>
 
 #include "store/database_t.h"
+#include "cli/arg_parser_t.h"
 
 #include "io/deserializer/database/stream/stream_database_deserializer_t.h"
 #include "io/deserializer/database/stream/old_format_stream_database_deserializer_t.h"
@@ -13,6 +16,10 @@
 
 int main(int argc, char *argv[])
 {
+    std::list<std::string> params(argv, argv + argc);
+    controller_t controller;
+    arg_parser_t(controller).parse(params);
+
     database_t db;
     std::ifstream ifs("persistence/save.txt");
     std::shared_ptr<stream_database_deserializer_t> deserializer(new old_format_stream_database_deserializer_t());
