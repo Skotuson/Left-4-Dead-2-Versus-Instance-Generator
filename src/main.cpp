81b0ac95738a6e192f46dfc37b6834cb2ea4fe23
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-25 19:47:13 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -37,10 +37,10 @@ int main(int argc, char *argv[])
     db_new = deserializer_new->deserialize(ifs_new);
     ifs_new.close();
 
-    auto saver_old = std::make_shared<file_stream_text_saver_t>("persistence/matches_old_format.txt");
+    auto saver_old = std::make_shared<file_stream_text_saver_t>("persistence/save.txt");
     saver_old->save(serializer_old->serialize(db_old));
 
-    auto saver_new = std::make_shared<file_stream_text_saver_t>("persistence/matches_new_format.txt");
+    auto saver_new = std::make_shared<file_stream_text_saver_t>("persistence/save_new.txt");
     saver_new->save(serializer_new->serialize(db_new));
     return 0;
 }
