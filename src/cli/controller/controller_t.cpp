d5aaa4d33a987e6e93b7bf3275991e20e1b7a01d
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-25 20:06:26 +0100

    create command queue in controller

--- src/cli/controller/controller_t.cpp
+++ src/cli/controller/controller_t.cpp
@@ -1,2 +1,6 @@
 #include "controller_t.h"
 
+void controller_t::push_command(std::shared_ptr<command_t> cmd)
+{
+    commands_.push(cmd);
+}
--- src/cli/controller/controller_t.h
+++ src/cli/controller/controller_t.h
@@ -4,9 +4,13 @@
 
 #include <string>
 #include <memory>
+#include <queue>
+
+#include "../../store/cmd/command_t.h"
 
 struct controller_t
 {
-
+    void push_command(std::shared_ptr<command_t> cmd);
 private:
+    std::queue<std::shared_ptr<command_t>> commands_;
 };
