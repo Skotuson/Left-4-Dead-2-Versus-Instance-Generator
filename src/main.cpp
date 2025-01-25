9479bb575c28595b1bfdb9d8b8b3fb1ff5112453
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-25 15:26:56 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@
 #include "io/deserializer/database/stream/old_format_stream_database_deserializer_t.h"
 
 #include "io/serializer/database/string/old_format_string_database_serializer_t.h"
+#include "io/serializer/database/string/new_format_string_database_serializer_t.h"
+
+#include "io/saver/text/file_stream_text_saver_t.h"
 
 int main(int argc, char *argv[])
 {
@@ -21,13 +24,16 @@ int main(int argc, char *argv[])
     database_t db;
     std::ifstream ifs("persistence/save.txt");
     std::shared_ptr<stream_database_deserializer_t> deserializer(new old_format_stream_database_deserializer_t());
-    std::shared_ptr<string_database_serializer_t> serializer(new old_format_string_database_serializer_t());
+    std::shared_ptr<string_database_serializer_t> serializer_old(new old_format_string_database_serializer_t());
+    std::shared_ptr<string_database_serializer_t> serializer_new(new new_format_string_database_serializer_t());
 
     db = deserializer->deserialize(ifs);
     ifs.close();
 
-    std::ofstream ofs("persistence/matches_old_format.txt");
-    ofs << serializer->serialize(db);
-    ofs.close();
+    auto saver_old = std::make_shared<file_stream_text_saver_t>("persistence/matches_old_format.txt");
+    saver_old->save(serializer_old->serialize(db));
+
+    auto saver_new = std::make_shared<file_stream_text_saver_t>("persistence/matches_new_format.txt");
+    saver_new->save(serializer_new->serialize(db));
     return 0;
 }
