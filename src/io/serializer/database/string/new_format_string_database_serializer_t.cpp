04309c0113864770f3726f78e5d9e5c733fd7f30
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-25 15:26:53 +0100

    new format serializer

---
+++ src/io/serializer/database/string/new_format_string_database_serializer_t.cpp
@@ -0,0 +1,25 @@
+#include "new_format_string_database_serializer_t.h"
+
+#include <sstream>
+
+std::string new_format_string_database_serializer_t::serialize(database_t const &database) const
+{
+    std::ostringstream result("");
+
+    auto outcome_to_string = [](match_t::outcome_t outcome)
+    {
+        return outcome == match_t::outcome_t::FIRST_TEAM_WON ? "F" : "S";
+    };
+
+    for (auto const &match : database.matches_get())
+    {
+        result << match.first() << " x "
+               << match.second() << " - "
+               << outcome_to_string(match.outcome())
+               << " " << match.map()
+               << " " << match.timestamp().time_since_epoch().count()
+               << std::endl;
+    }
+
+    return result.str();
+}
