0699ceb6c9dc93f7ed14589bd81ab8549ee0e119
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-01-25 19:19:40 +0100

    add new format loading

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "cli/arg_parser_t.h"
 
 #include "io/deserializer/database/stream/old_format_stream_database_deserializer_t.h"
+#include "io/deserializer/database/stream/new_format_stream_database_deserializer_t.h"
 
 #include "io/serializer/database/string/old_format_string_database_serializer_t.h"
 #include "io/serializer/database/string/new_format_string_database_serializer_t.h"
@@ -21,19 +22,25 @@ int main(int argc, char *argv[])
     controller_t controller;
     arg_parser_t(controller).parse(params);
 
-    database_t db;
-    std::ifstream ifs("persistence/save.txt");
-    std::shared_ptr<stream_database_deserializer_t> deserializer(new old_format_stream_database_deserializer_t());
+    std::shared_ptr<stream_database_deserializer_t> deserializer_old(new old_format_stream_database_deserializer_t());
+    std::shared_ptr<stream_database_deserializer_t> deserializer_new(new new_format_stream_database_deserializer_t());
     std::shared_ptr<string_database_serializer_t> serializer_old(new old_format_string_database_serializer_t());
     std::shared_ptr<string_database_serializer_t> serializer_new(new new_format_string_database_serializer_t());
 
-    db = deserializer->deserialize(ifs);
-    ifs.close();
+    database_t db_old;
+    std::ifstream ifs_old("persistence/save.txt");
+    db_old = deserializer_old->deserialize(ifs_old);
+    ifs_old.close();
+
+    database_t db_new;
+    std::ifstream ifs_new("persistence/save_new.txt");
+    db_new = deserializer_new->deserialize(ifs_new);
+    ifs_new.close();
 
     auto saver_old = std::make_shared<file_stream_text_saver_t>("persistence/matches_old_format.txt");
-    saver_old->save(serializer_old->serialize(db));
+    saver_old->save(serializer_old->serialize(db_old));
 
     auto saver_new = std::make_shared<file_stream_text_saver_t>("persistence/matches_new_format.txt");
-    saver_new->save(serializer_new->serialize(db));
+    saver_new->save(serializer_new->serialize(db_new));
     return 0;
 }
