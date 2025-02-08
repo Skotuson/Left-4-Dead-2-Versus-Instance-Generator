f37da0481e7adc6e6e3657a33f06172dcf13a287
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-02-08 01:11:32 +0100

    implement add handler

--- src/cli/cor/param/cmd/add_handler_t.cpp
+++ src/cli/cor/param/cmd/add_handler_t.cpp
@@ -2,8 +2,11 @@
 
 #include <memory>
 #include <vector>
+#include <sstream>
+#include <numeric>
 
 #include "../../../../io/deserializer/database/stream/new_format_stream_database_deserializer_t.h"
+#include "../../../../store/cmd/mut/add_match_command_t.h"
 
 add_handler_t::add_handler_t(std::string const &flag, controller_t &controller)
     : param_handler_t(flag, controller)
@@ -14,13 +17,22 @@ std::optional<std::shared_ptr<handler_t<_PARAM_HANDLER_PAIR_>>> add_handler_t::p
 {
     if (args.empty())
     {
-        // TODO
+        // TODO: error handling
+        throw "";
         // return std::make_shared<error_handler_t<_PARAM_HANDLER_PAIR_>>(flag_, "No input file provided");
     }
 
-    std::shared_ptr<command_t> cmd;
+    std::shared_ptr<new_format_stream_database_deserializer_t> deserializer(std::make_shared<new_format_stream_database_deserializer_t>());
 
-    controller_.push_command(cmd);
+    std::istringstream iss(
+        std::accumulate(std::next(args.begin()), args.end(), args.front(),
+                        [](std::string const &a, std::string const &b)
+                        {
+                            return a + " " + b;
+                        }));
+
+    controller_.push_command(std::make_shared<add_match_command_t>(
+        deserializer->deserialize(iss).matches_get().front()));
     return std::nullopt;
 }
 
