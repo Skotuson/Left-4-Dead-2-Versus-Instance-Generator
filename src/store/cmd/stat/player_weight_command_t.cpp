3b2671154a51b319d5a487f57917ec37bcadbfd9
Jiří Skotal <58478268+Skotuson@users.noreply.github.com>
2025-02-08 21:08:01 +0100

    add weight_algorithm enum

--- src/store/cmd/stat/player_weight_command_t.cpp
+++ src/store/cmd/stat/player_weight_command_t.cpp
@@ -1,7 +1,9 @@
 #include "player_weight_command_t.h"
 
-player_weight_command_t::player_weight_command_t(player_t const &player)
-    : player_(player)
+player_weight_command_t::player_weight_command_t(player_t const &player,
+                                                 player_weight_command_t::weight_algorithm_t const &weight_algorithm)
+    : player_(player),
+      weight_algorithm_(weight_algorithm)
 {
 }
 
--- src/store/cmd/stat/player_weight_command_t.h
+++ src/store/cmd/stat/player_weight_command_t.h
@@ -5,9 +5,17 @@
 
 struct player_weight_command_t : command_t
 {
-    player_weight_command_t(player_t const & player);
+    enum weight_algorithm_t
+    {
+        WINRATE,
+        WEIGHTED,
+        ELO
+    };
+
+    player_weight_command_t(player_t const &player, weight_algorithm_t const &weight_algorithm = WINRATE);
     void execute(database_t &db) const;
 
 private:
     player_t player_;
+    weight_algorithm_t weight_algorithm_;
 };
